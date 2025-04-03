455a95fa97038080869efdacc184d4ba329708c2
liuqinglin <liuqinglin@wekoi.cn>
2025-04-03 19:02:53 +0800

    增加infra-logging模块

---
+++ infra-core/src/main/java/io/infra/structure/core/tool/ApiData.java
@@ -0,0 +1,50 @@
+package io.infra.structure.core.tool;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author liuqinglin
+ * Date: 2025/4/2 上午11:09
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ApiData<T> {
+    /**
+     * 错误码
+     */
+    private int code;
+
+    /**
+     * 错误描述
+     */
+    private String msg;
+
+    /**
+     * 数据
+     */
+    private T data;
+
+    public static <T> ApiData<T> success(T data) {
+        return ApiData.<T>builder()
+                .code(0)
+                .msg("success")
+                .data(data)
+                .build();
+    }
+
+    public static <T> ApiData<T> success() {
+        return success(null);
+    }
+
+    public static <T> ApiData<T> fail(int code, String msg) {
+        return ApiData.<T>builder()
+                .code(code)
+                .msg(msg)
+                .build();
+    }
+}
---
+++ infra-logging/src/main/java/io/infra/logging/converter/ThreadColorConverter.java
@@ -0,0 +1,15 @@
+package io.infra.logging.converter;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.pattern.color.ForegroundCompositeConverterBase;
+
+/**
+ * @author liuqinglin
+ * Date: 2025/4/2 17:33
+ */
+public class ThreadColorConverter extends ForegroundCompositeConverterBase<ILoggingEvent> {
+    @Override
+    protected String getForegroundColorCode(ILoggingEvent event) {
+        return "38;2;96;175;96";
+    }
+}
