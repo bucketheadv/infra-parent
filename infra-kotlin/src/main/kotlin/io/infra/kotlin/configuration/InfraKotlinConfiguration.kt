746f3e0ef52345d08e96a33340bc6e41c4f59ed2
liuqinglin <liuqinglin@wekoi.cn>
2025-04-19 22:57:24 +0800

    优化kotlin依赖

--- infra-kotlin/src/main/kotlin/io/infra/kotlin/configuration/InfraKotlinConfiguration.kt
+++ infra-kotlin/src/main/kotlin/io/infra/kotlin/configuration/InfraKotlinConfiguration.kt
@@ -1,11 +1,12 @@
 package io.infra.kotlin.configuration
 
-//import org.springframework.context.annotation.ComponentScan
-//import org.springframework.context.annotation.Configuration
-//
-///**
-// * @author liuqinglin
-// * Date: 2025/4/19 22:05
-// */
-//@ComponentScan("io.infra.kotlin")
-//class InfraKotlinConfiguration
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+/**
+ * @author liuqinglin
+ * Date: 2025/4/19 22:05
+ */
+@Configuration
+@ComponentScan("io.infra.kotlin")
+class InfraKotlinConfiguration
